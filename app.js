66e5a3144ee2d3d00ab14eb4c0d8a6021cdc1082
nizar <nizarucsc@gmail.com>
2017-08-28 17:57:37 +0530

    working app

--- app.js
+++ app.js
@@ -1,80 +1,87 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // twitter post sharing app
+    var quotearea = document.getElementById('artical')
+    var output = document.getElementById('output')
+    var tooltiptext = document.createElement('span');
+    var twitterUrl = 'https://twitter.com/intent/tweet?text=';
 
-// twitter post sharing app
-var quotearea = document.getElementById('artical')
-var output = document.getElementById('output')
-var tooltiptext = document.createElement('span');
-var twitterUrl = 'https://twitter.com/intent/tweet?text=';
-
-/**
- * helper function to pop window
- * @param {*} url 
- */
-function popitup(url) {
-    newwindow = window.open(url, 'name', 'height=400,width=600,top=200,left=400');
-    if (window.focus) { newwindow.focus() }
-    return false;
-}
-/**
- * helper funtionto set tooltip
- * @param {*} tooltiptext 
- * @param {*} sel 
- */
-function setToolTip(tooltiptext, sel) {
-    var range = sel.getRangeAt(0),        // the range at first selection group
-        rect = range.getBoundingClientRect();
-    this.tooltiptext = tooltiptext;
-    this.tooltiptext.style.position = 'fixed';              // fixed positioning = easy mode
-    this.tooltiptext.style.top = rect.top - 40 + 'px';       // set coordinates
-    this.tooltiptext.style.left = rect.left + 'px';
-    this.tooltiptext.style.height = rect.height + 'px'; // and size
-    this.tooltiptext.style.width = rect.width + 'px';
-    quotearea.appendChild(tooltiptext);
-    var text = sel.toString() + '  ' + window.location.href
-    var shareButton = document.getElementById('share-button');
-    shareButton.addEventListener('mouseup', function () {
-        tooltiptext.style.display = 'none';
-        popitup(twitterUrl + text);
-    })
-}
-
-
-// listen the selection add popup the tooltip for share
-quotearea.addEventListener('mouseup', function () {
-    var node = ''
-    var tooltipSpan = document.getElementsByClassName('tooltiptext');
-
-    if (document.getSelection) {
-        // all browsers, except IE before version 9
-        var sel = document.getSelection(),
-            range = sel.getRangeAt(0),        // the range at first selection group
-            rect = range.getBoundingClientRect(); // and convert this to useful data
-        var text = sel.toString();
-        if (140 >= text.length > 0) {
-            tooltiptext.innerHTML = '<span class="tooltiptext"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i><a class="twitter-share-button"  > <i class="fa fa-twitter fa-lg" id="share-button"  aria-hidden="true"></i></a></span>';
-            setToolTip(tooltiptext, sel);
-        } else {
-            tooltiptext.innerHTML = '<span class="tooltiptext">Selected text is grater than sharble content on tweet</span> '
-            setToolTip(tooltiptext, sel);
-        }
-
+    /**
+     * helper function to pop window
+     * @param {*} url 
+     */
+    function popitup(url) {
+        newwindow = window.open(url, 'name', 'height=400,width=600,top=200,left=400');
+        if (window.focus) { newwindow.focus() }
+        return false;
     }
-    else {
-        if (document.selection) {   // Internet Explorer before version 9
-            var textRange = document.selection.createRange();
-            if (140 >= sel.toString().length > 0) {
-                tooltiptext.innerHTML = '<span class="tooltiptext"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i><a class="twitter-share-button" > <i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></span>';
+    /**
+     * helper funtionto set tooltip
+     * @param {*} tooltiptext 
+     * @param {*} sel 
+     */
+    function setToolTip(tooltiptext, sel) {
+        var range = sel.getRangeAt(0),        // the range at first selection group
+            rect = range.getBoundingClientRect();
+        this.tooltiptext = tooltiptext;
+        this.tooltiptext.style.position = 'fixed';              // fixed positioning = easy mode
+        this.tooltiptext.style.top = rect.top - 40 + 'px';       // set coordinates
+        this.tooltiptext.style.left = rect.left + 'px';
+        this.tooltiptext.style.height = rect.height + 'px'; // and size
+        this.tooltiptext.style.width = rect.width + 'px';
+        quotearea.appendChild(this.tooltiptext);
+        var text = sel.toString() + '  ' + window.location.href
+        var shareButton = document.getElementById('share-button');
+        if (shareButton !== null) {
+            shareButton.addEventListener('mouseup', function () {
+                popitup(twitterUrl + text);
+            })
+        }
+    }
+
+
+    function select() {
+        // listen the selection add popup the tooltip for share
+        var node = ''
+        var tooltipSpan = document.getElementsByClassName('tooltiptext');
+
+        if (document.getSelection) {
+            // all browsers, except IE before version 9
+            var sel = document.getSelection(),
+                range = sel.getRangeAt(0),        // the range at first selection group
+                rect = range.getBoundingClientRect(); // and convert this to useful data
+            var text = sel.toString();
+            if (140 >= text.length > 0) {
+                tooltiptext.innerHTML = '<span class="tooltiptext"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i><a class="twitter-share-button"  > <i class="fa fa-twitter fa-lg" id="share-button"  aria-hidden="true"></i></a></span>';
+                setToolTip(tooltiptext, sel);
+            } else {
+                tooltiptext.innerHTML = '<span class="tooltiptext">Selected text is grater than sharble content on tweet</span> '
                 setToolTip(tooltiptext, sel);
             }
-        } else {
-            tooltiptext.innerHTML = '<span class="tooltiptext">Selected text is grater than sharble content on tweet</span> '
-            setToolTip(tooltiptext, sel);
+
+        }
+        else {
+            if (document.selection) {   // Internet Explorer before version 9
+                var textRange = document.selection.createRange();
+                if (140 >= sel.toString().length > 0) {
+                    tooltiptext.innerHTML = '<span class="tooltiptext"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i><a class="twitter-share-button" > <i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></span>';
+                    setToolTip(tooltiptext, sel);
+                }
+            } else {
+                tooltiptext.innerHTML = '<span class="tooltiptext">Selected text is grater than sharble content on tweet</span> '
+                setToolTip(tooltiptext, sel);
+            }
         }
     }
-}, false)
 
-// reset 
-quotearea.addEventListener('mouseleave', function () {
-    tooltiptext.innerHTML = '';
+    quotearea.addEventListener('mouseup', function () {
+        select();
+    })
+
+
+    // quotearea.addEventListener('mouseleave', function () {
+    //     tooltiptext.innerHTML = null;
+    // });
+
 });
 
 
